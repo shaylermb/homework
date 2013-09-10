9784531dd27a8b09f27cc77c220ef98d87b481fe
shaylermb <shaylermb@s.dcsdk12.org>
2013-09-09 21:18:43 -0600

    initial with factorial

---
+++ homework.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class 19Exercise {
+	public static void main(String[] args) {
+		Scanner reader = new Scanner; 
+		int p = reader.nextInt;
+		e(p);
+	}
+	public static void fac(int x){
+		int y = x
+		while (x>0) {
+			y = y*(x-1);
+			x--; 
+		}
+	}
+blah
